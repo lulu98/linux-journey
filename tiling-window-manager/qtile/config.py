071596b2545ad28db91da0701a53a43863a01b47
lukas <lukas-graber@protonmail.com>
2020-08-26 14:20:24 +0200

    more config settings

--- tiling-window-manager/qtile/config.py
+++ tiling-window-manager/qtile/config.py
@@ -37,43 +37,86 @@ mod = "mod4"
 terminal = "terminator"
 
 keys = [
-    # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down(),
+    Key([mod], 
+        "k", 
+        lazy.layout.down(),
         desc="Move focus down in stack pane"),
-    Key([mod], "j", lazy.layout.up(),
+    
+    Key([mod], 
+        "j", 
+        lazy.layout.up(),
         desc="Move focus up in stack pane"),
 
-    # Move windows up or down in current stack
-    Key([mod, "control"], "k", lazy.layout.shuffle_down(),
+    Key([mod, "control"], 
+        "k", 
+        lazy.layout.shuffle_down(),
         desc="Move window down in current stack "),
-    Key([mod, "control"], "j", lazy.layout.shuffle_up(),
+    
+    Key([mod, "control"], 
+        "j", 
+        lazy.layout.shuffle_up(),
         desc="Move window up in current stack "),
 
-    # Switch window focus to other pane(s) of stack
-    Key([mod], "space", lazy.layout.next(),
+    Key([mod], 
+        "space", 
+        lazy.layout.next(),
         desc="Switch window focus to other pane(s) of stack"),
 
-    # Swap panes of split stack
-    Key([mod, "shift"], "space", lazy.layout.rotate(),
+    Key([mod, "shift"], 
+        "space", 
+        lazy.layout.rotate(),
         desc="Swap panes of split stack"),
 
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+    Key([mod, "shift"], 
+        "Return", 
+        lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod, "control"], "l", lazy.spawn(os.path.join(os.path.dirname(__file__), 'bin/lock'))),
 
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], 
+        "Return", 
+        lazy.spawn(terminal), 
+        desc="Launch terminal"),
 
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
-    Key([mod], "r", lazy.spawncmd(),
+    Key([mod], 
+        "Tab", 
+        lazy.next_layout(), 
+        desc="Toggle between layouts"),
+
+    Key([mod], 
+        "w", 
+        lazy.window.kill(), 
+        desc="Kill focused window"),
+
+    Key([mod, "control"], 
+        "r", 
+        lazy.restart(), 
+        desc="Restart qtile"),
+
+    Key([mod, "control"], 
+        "q", 
+        lazy.shutdown(), 
+        desc="Shutdown qtile"),
+    
+    Key([mod], 
+        "r", 
+        lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"),
+    
+    Key([mod, "control"], 
+        "l", 
+        lazy.spawn(os.path.join(os.path.dirname(__file__), 'bin/lock')), 
+        desc="Lock screen"),
+
+    #########################
+    #     SCREENSHOTS       #
+    #########################
+    Key([mod, "shift"], "Print", lazy.spawn('gnome-screenshot -i')),
+
+    #########################
+    # CONTROL + ALT KEYS    #
+    #########################
+    Key(["mod1", "control"], "v", lazy.spawn('code')),
+    Key(["mod1", "control"], "f", lazy.spawn('firefox')),
 ]
 
 group_names = [("WWW", {'layout': 'max'}),
