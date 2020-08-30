cf188b6cdafbc6242c5fc5f3cd068ecd062f504d
lukas graber <lukas-graber@protonmail.com>
2020-08-30 19:40:07 +0200

    more on install script and .zshrc

--- install_script.sh
+++ install_script.sh
@@ -1,53 +1,67 @@
 #!/usr/bin/env bash
 # this install script installs my entire setup
+echo 'update and upgrade'
 sudo apt-get update
 sudo apt-get upgrade
 
 # install curl and wget
+echo 'install curl and wget'
 sudo apt-get install wget curl
+
+# install silver searcher
+echo 'install silversearcher'
+sudo apt-get install silversearcher-ag
+
 # install terminator
+echo 'install terminator'
 sudo apt-get install terminator
 
 # install git
+echo 'install git'
 sudo apt-get install git
+
+# install latex
+echo 'install latex'
+sudo apt-get install miktex texlive-full texlive-fonts-extra
+
+# install java
+echo 'install java-jdk'
+sudo apt-get install openjdk-8-jre
+
+# install python3
+echo 'install python3'
+sudo apt-get install python3
+sudo apt-get install python3-dev cmake
 
 # clone github repo
 repo_path=~/Desktop/linux-journey
 git clone git@github.com:lulu98/linux-journey.git $repo_path 
 
 # install zsh
+echo 'install and setup of zsh'
 sudo apt-get install zsh
-chsh -s /usr/bin/zsh root
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+sudo usermod -s /usr/bin/zsh $(whoami)
 sudo apt-get install powerline fonts-powerline
 sudo apt-get install zsh-theme-powerlevel9k
 sudo apt-get install zsh-syntax-highlighting
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cat ${repo_path}/zshrc > ~/.zshrc
 source ~/.zshrc 
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# install latex
-sudo apt-get install miktex
-sudo apt-get install texlive-full
-sudo apt-get install texlive-fonts-extra
-
-# install java
-sudo apt-get install openjdk-8-jre
-
-# install python3
-sudo apt-get install python3
-sudo apt-get install python3-dev cmake
-
-# install riot
-sudo apt-get install -y lsb-release apt-transport-https
-sudo wget -O /usr/share/keyrings/riot-im-archive-keyring.gpg https://packages.riot.im/debian/riot-im-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/riot-im-archive-keyring.gpg] https://packages.riot.im/debian/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/riot-im.list
-sudo apt-get update
-sudo apt-get install riot-web
+#zsh
+#sudo apt-get install powerline fonts-powerline
+#sudo apt-get install zsh-theme-powerlevel9k
+#echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
+#sudo apt-get install zsh-syntax-highlighting
+#echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
+#echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+#source ~/.zshrc 
 
 ###########################
 ## install and setup vim ##
 ###########################
+echo 'install and setup of vim'
 sudo apt-get install vim
 sudo apt-get install vim-gtk
 rm -rf ~/vim
@@ -62,27 +76,33 @@ vim -c 'PluginInstall' -c 'qa!'
 ################
 ## install i3 ##
 ################
+echo 'install and setup of i3'
 sudo apt-get install i3
 ## setup
 cp -r ${repo_path}/config/i3 ~/.config 
 
 ## additional software
 ### install feh for wallpapers
+echo 'install of feh'
 sudo apt-get install feh
 cp -r ${repo_path}/wallpapers ~/Pictures
 echo 'feh --bg-fill ~/Pictures/wallpapers/wallpaper01.jpg' >> ~/.profile
 
 ### install xscreensaver
+echo 'install of xscreensaver'
 sudo apt-get remove gnome-screensaver
 sudo apt-get install xscreensaver xscreensaver-data-extra xscreensaver-gl-extra
 
 ### install vi file manager (vifm)
+echo 'install of vifm'
 sudo apt-get install vifm
 
 ### install rofi application launcher
+echo 'install of rofi'
 sudo apt-get install rofi
 
 ### install and setup polybar
+echo 'install and setup of polybar'
 #### install build dependencies
 sudo apt-get install build-essential clang-7.0 cmake cmake-data pkg-config python3 python3-sphinx
 #### isntall dependencies
@@ -100,4 +120,3 @@ make -j4
 sudo make install
 #### copy config
 cp -r ${repo_path}/config/polybar ~/.config 
-#i3-msg restart
